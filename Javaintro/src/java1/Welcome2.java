4023c31055cff0861ac6283836a17b1ef4be523a
siva <chitt@Sivaram>
2026-05-22 18:23:39 +0530

    about mathods and blocks

--- Javaintro/src/java1/Welcome2.java
+++ Javaintro/src/java1/Welcome2.java
@@ -1,10 +1,32 @@
 package java1;
 
 public class Welcome2 {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("welcome to my world");
+	//	m1();
+		Welcome2 t1 = new Welcome2();
+		t1.m2();
+		
+	}
+	
+	static  void m1(){
+		System.out.println("welcome to my world2");
+		
+	}
+	
+	void m2(){
+		System.out.println("welcome to my world3");
+		m3();
+		m1();
+	
+	}
+	
+	void m3(){
+		System.out.println("welcome to my world 4");
+		
+		
 	}
 
 }
---
+++ Javaintro/src/java1/Welcome3.java
@@ -0,0 +1,32 @@
+package java1;
+
+public class Welcome3 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to my world 1 ");
+		Welcome3 t2 = new Welcome3();
+	}
+	
+	static {
+		System.out.println("welcome to my world 2 ");
+		
+	}
+	
+	static {
+		System.out.println("welcome to my world 3 ");
+		//Welcome3 t2 = new Welcome3();
+		
+	}
+	
+	{
+			System.out.println("welcome to my world 4 ");
+			//Welcome3 t2 = new Welcome3();
+			
+		}
+	 
+	//Welcome3 t2 = new Welcome3();
+	
+	}
+
+
+
